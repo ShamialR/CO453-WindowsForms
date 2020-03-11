a6d0720e34264da602886b8c2ca16b2fa9aabe26
ShamialR <Shamial_r@hotmail.com>
2020-03-11 16:20:58 +0000

    Task 1.2 DataEntryForm Completed

--- WindowsForms/Unit1/DataEntryForm.cs
+++ WindowsForms/Unit1/DataEntryForm.cs
@@ -19,7 +19,37 @@ namespace WindowsForms.Unit1
 
         private void messageLabel_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void messageButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = "Hi " + firstNameText.Text + " " + lastNameText.Text + "\nHappy Programming";
+        }
+
+        private void lastNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = " ";
+        }
+
+        private void toUpperButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = messageLabel.Text.ToUpper();
+        }
+
+        private void toLowerButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = messageLabel.Text.ToLower(); 
         }
     }
 }
