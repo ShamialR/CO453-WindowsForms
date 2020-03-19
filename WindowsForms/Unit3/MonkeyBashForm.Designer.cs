e58a98f71844a29d6a897182d18b12328649f16b
ShamialR <Shamial_r@hotmail.com>
2020-03-19 16:24:32 +0000

    Monkey Bash Form progress

---
+++ WindowsForms/Unit3/MonkeyBashForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsForms.Unit3
+{
+    partial class MonkeyBashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MonkeyBashForm));
+            this.monkeyPictureBox = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.animationTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.monkeyPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monkeyPictureBox
+            // 
+            this.monkeyPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("monkeyPictureBox.Image")));
+            this.monkeyPictureBox.Location = new System.Drawing.Point(66, 47);
+            this.monkeyPictureBox.Name = "monkeyPictureBox";
+            this.monkeyPictureBox.Size = new System.Drawing.Size(83, 91);
+            this.monkeyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.monkeyPictureBox.TabIndex = 0;
+            this.monkeyPictureBox.TabStop = false;
+            this.monkeyPictureBox.Click += new System.EventHandler(this.monkeyPictureBox_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 262);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(146, 262);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(281, 262);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 3;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // animationTimer
+            // 
+            this.animationTimer.Tick += new System.EventHandler(this.animationTimer_Tick);
+            // 
+            // MonkeyBashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(564, 297);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.monkeyPictureBox);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "MonkeyBashForm";
+            this.Text = "MonkeyBashForm";
+            ((System.ComponentModel.ISupportInitialize)(this.monkeyPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox monkeyPictureBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Timer animationTimer;
+    }
+}
---
+++ WindowsForms/Unit3/MonkeyBashForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms.Unit3
+{
+    public partial class MonkeyBashForm : Form
+    {
+
+        private int x = 0, y = 0, playerScore = 0, monkeyScore = 0;
+        private Random generator = new Random();
+
+
+        public MonkeyBashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void monkeyPictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ouch! You HIT Me!", "Monkey");
+            monkeyPictureBox.Image = Image.FromFile("monkeySad.jpg");
+        }
+
+        private void animationTimer_Tick(object sender, EventArgs e)
+        {
+            x = generator.Next(this.Width - 100);
+            y = generator.Next(this.Height - 100);
+            monkeyPictureBox.Left = x;
+            monkeyPictureBox.Top = y;
+            Refresh();
+
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            animationTimer.Enabled = true;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            animationTimer.Enabled = true;  
+        }
+
+    }
+}
